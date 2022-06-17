6544a02b18a3859b2305ce1b6dd392fdbfb6e41a
hle0 <91701075+hle0@users.noreply.github.com>
2022-06-17 13:29:01 -0400

    modify voice training script layout
    Signed-off-by: hle0 <91701075+hle0@users.noreply.github.com>

--- base.sh
+++ base.sh
@@ -25,6 +25,10 @@ fn_exists com-sig || com-sig() {
     echo "$(whoami)@$(hostname): $(date)"
 }
 
+beep() {
+    tput bel
+}
+
 # check differences
 chkdiff() {
     if [ $(git status --porcelain -uno | wc -l) -gt 0 ]; then
@@ -53,30 +57,57 @@ qcom() {
     fi
 }
 
+LAST_DIARY100_FILE=""
+
 # quick edit
 qed() {
     nano "$1"
+
+    maybe_add "$1"
+
+    return $?
+}
+
+maybe_add() {
     if dialog --yesno "Commit changes to this file?" 0 0; then
-        git add "$1" && ecom
+	LAST_DIARY100_FILE="$1"
+        git add "$LAST_DIARY100_FILE" && ecom
+	return $?
     fi
+
+    return 1
 }
 
 fn_exists audio_filename || audio_filename() {
     echo "audio/$(date -u +%F_%H_%M_%S)-$1.${audio_ext}"
 }
 
+qrec() {
+    echo "Press Ctrl-C to stop"
+    arecord -c 2 -r 96000 "$1.tmp"
+    ffmpeg -loglevel error -f wav -i "$1.tmp" -af loudnorm "$1"
+    rm "$1.tmp"
+}
+
+# quick play (autoexit)
+qplay() {
+    ffplay -autoexit "$1"
+}
+
+# long play
+lplay() {
+    vlc "file:///$(pwd)/$1"
+}
+
 # quick audio
 qad() {
     newfilename="$(audio_filename "$1")"
-    echo "Press Ctrl-C to stop"
-    arecord -c 2 -r 96000 "$newfilename.tmp"
-    ffmpeg -loglevel error -f wav -i "$newfilename.tmp" -af loudnorm "$newfilename"
-    rm "$newfilename.tmp"
+    qrec "$newfilename"
 
-    vlc "file:///$(pwd)/$newfilename"
-    if dialog --yesno "Commit changes to this file?" 0 0; then
-        git add "$newfilename" && ecom
-    else
+    lplay "$newfilename"
+
+    if ! maybe_add "$newfilename"; then
 	rm "$newfilename"
+	return 1
     fi
 }
